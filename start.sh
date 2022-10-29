23d4fddb60b2e61950bf5521505fc63f1ac94b74
kkhanyaseen <91598415+kkhanyaseen@users.noreply.github.com>
2022-10-29 22:45:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/abke-tuje-dund-lete-hai /MdiskBot
-cd /MdiskBot
+git clone https://github.com/AnnaGophane/abke-tuje-dund-lete-hai /Mdisk
+cd /Mdisk
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
